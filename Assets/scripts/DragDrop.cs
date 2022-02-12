717230cc4a98b10da611b9f61653067bb3501b4a
Joseph Corey <jstephencorey@gmail.com>
2022-02-12 12:49:10 -0700

    Can play cards from multiple players

--- Assets/scripts/DragDrop.cs
+++ Assets/scripts/DragDrop.cs
@@ -4,28 +4,60 @@ using UnityEngine;
 
 public class DragDrop : MonoBehaviour
 {
+    public GameObject Canvas;
+    public GameObject DropZone;
 
-    private bool isDragging = false;
+    private bool isDragging;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject dropZone;
+    private bool isOverDropZone;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        Canvas = GameObject.Find("Main Canvas");
+        DropZone = GameObject.Find("DropZone");
     }
 
-    public void beginDrag() {
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+    }
+
+    private void OnCollisionExit2D(Collision2D collision) 
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    public void beginDrag()
+    {
         isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
     }
 
-    public void endDrag() {
+    public void endDrag()
+    {
         isDragging = false;
+        if (isOverDropZone)
+        {
+            transform.SetParent(dropZone.transform, false);
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (isDragging) {
+        if (isDragging)
+        {
             transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
         }
     }
 }
