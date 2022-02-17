78e625625d928d267c2d46dc53fd5c56f9bd0b28
Benjamin Jacobs <benjammin1100@gmail.com>
2022-02-17 00:50:07 -0700

    Autogenerate cards programatically

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,13 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+public enum CardColor
+{
+    GREEN = 0,
+    ORANGE = 14,
+    RED = 28,
+    BLACK = 42,
+    ROOK = 56,
+}
 
 public class Card : MonoBehaviour
 {
+
+    public Texture2D texture;
+    public int width;
+    public int height;
+
+    private CardColor color = CardColor.GREEN;
+    private int number = 1;
+
+    private int id = 0;
+
+    public void SetCard(CardColor color, int number)
+    {
+        this.color = color;
+        this.number = number;
+        id = ((int)color) + number - 1;
+        loadImage();
+    }
+
+    public CardColor GetColor()
+    {
+        return color;
+    }
+
+    public int GetNumber()
+    {
+        return number;
+    }
+
+    private void loadImage()
+    {
+        int x = id % width;
+        int y = id / width;
+        int w = texture.width / width;
+        int h = texture.height / height;
+        Rect rect = new Rect(x * w, texture.height - y * h - h, w, h);
+
+        Sprite sprite = Sprite.Create(texture, rect, new Vector2(0.5f, 0.5f));
+        Image i = gameObject.GetComponent<Image>();
+        i.sprite = sprite;
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
--- Assets/scripts/Deck.cs
+++ Assets/scripts/Deck.cs
@@ -2,17 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Deck : MonoBehaviour
 {
 
-    public GameObject[] cards;
+    public GameObject card;
     public GameObject hand1;
     public GameObject hand2;
     public GameObject hand3;
     public GameObject hand4;
 
-    private List<GameObject> deck = new List<GameObject>();
+    private readonly List<CardData> deck = new List<CardData>();
+
+    private class CardData
+    {
+        public CardColor color;
+        public int number;
+
+        public CardData(CardColor color, int number)
+        {
+            this.color = color;
+            this.number = number;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -29,21 +40,45 @@ public class Deck : MonoBehaviour
     // Draw a single card from the deck
     public GameObject Draw(Quaternion rotation)
     {
-        GameObject toInit = deck[deck.Count - 1];
+        if (deck.Count == 0)
+        {
+            Debug.Log("No more cards");
+            return null;
+        }
+        CardData toInit = deck[deck.Count - 1];
         deck.RemoveAt(deck.Count - 1);
-        return Instantiate(toInit, new Vector2(0, 0), rotation);
+        GameObject obj = Instantiate(card, new Vector2(0, 0), rotation);
+        Card c = obj.GetComponent<Card>();
+        c.SetCard(toInit.color, toInit.number);
+        return obj;
     }
 
     // Deal 1 card to each player
     public void Deal()
     {
         GameObject card1 = Draw(Quaternion.identity);
+        if (card1 == null)
+        {
+            return;
+        }
         card1.transform.SetParent(hand1.transform, false);
         GameObject card2 = Draw(Quaternion.Euler(x:0, y:0, z:90));
+        if (card2 == null)
+        {
+            return;
+        }
         card2.transform.SetParent(hand2.transform, false);
         GameObject card3 = Draw(Quaternion.Euler(x:0, y:0, z:180));
+        if (card3 == null)
+        {
+            return;
+        }
         card3.transform.SetParent(hand3.transform, false);
         GameObject card4 = Draw(Quaternion.Euler(x:0, y:0, z:270));
+        if (card4 == null)
+        {
+            return;
+        }
         card4.transform.SetParent(hand4.transform, false);
     }
 
@@ -55,7 +90,7 @@ public class Deck : MonoBehaviour
         {
             n--;
             int k = Random.Range(0, n);
-            GameObject value = deck[k];
+            CardData value = deck[k];
             deck[k] = deck[n];
             deck[n] = value;
         }
@@ -68,10 +103,27 @@ public class Deck : MonoBehaviour
     public void Restock()
     {
         deck.Clear();
-        foreach (GameObject card in cards)
+        CardColor color = CardColor.GREEN;
+        for (int i = 1; i <= 14; ++i)
         {
-            deck.Add(card);
+            deck.Add(new CardData(color, i));
         }
+        color = CardColor.ORANGE;
+        for (int i = 1; i <= 14; ++i)
+        {
+            deck.Add(new CardData(color, i));
+        }
+        color = CardColor.RED;
+        for (int i = 1; i <= 14; ++i)
+        {
+            deck.Add(new CardData(color, i));
+        }
+        color = CardColor.BLACK;
+        for (int i = 1; i <= 14; ++i)
+        {
+            deck.Add(new CardData(color, i));
+        }
+        deck.Add(new CardData(CardColor.ROOK, 1));
     }
 
     // Update is called once per frame
