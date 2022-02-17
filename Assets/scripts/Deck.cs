3a256845ffa3609244eee7a503041cb2713e5337
Benjamin Jacobs <benjammin1100@gmail.com>
2022-02-16 22:50:44 -0700

    Add cards

--- Assets/scripts/Deck.cs
+++ Assets/scripts/Deck.cs
@@ -6,7 +6,11 @@ using UnityEngine;
 public class Deck : MonoBehaviour
 {
 
-    public GameObject[] cards; 
+    public GameObject[] cards;
+    public GameObject hand1;
+    public GameObject hand2;
+    public GameObject hand3;
+    public GameObject hand4;
 
     private List<GameObject> deck = new List<GameObject>();
 
@@ -23,11 +27,24 @@ public class Deck : MonoBehaviour
     }
 
     // Draw a single card from the deck
-    public void Draw()
+    public GameObject Draw(Quaternion rotation)
     {
         GameObject toInit = deck[deck.Count - 1];
         deck.RemoveAt(deck.Count - 1);
-        GameObject playerCard = Instantiate(toInit, new Vector2(0, 0), Quaternion.identity);
+        return Instantiate(toInit, new Vector2(0, 0), rotation);
+    }
+
+    // Deal 1 card to each player
+    public void Deal()
+    {
+        GameObject card1 = Draw(Quaternion.identity);
+        card1.transform.SetParent(hand1.transform, false);
+        GameObject card2 = Draw(Quaternion.Euler(x:0, y:0, z:90));
+        card2.transform.SetParent(hand2.transform, false);
+        GameObject card3 = Draw(Quaternion.Euler(x:0, y:0, z:180));
+        card3.transform.SetParent(hand3.transform, false);
+        GameObject card4 = Draw(Quaternion.Euler(x:0, y:0, z:270));
+        card4.transform.SetParent(hand4.transform, false);
     }
 
     // Shuffle the deck in place
