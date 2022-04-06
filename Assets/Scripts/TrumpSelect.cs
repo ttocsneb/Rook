cdc2a46fb894293f0f09426377eb4138b3767591
Benjamin Jacobs <benjammin1100@gmail.com>
2022-04-06 14:51:47 -0600

    Make trump select interactive

--- Assets/Scripts/TrumpSelect.cs
+++ Assets/Scripts/TrumpSelect.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TrumpSelect : MonoBehaviour
 {
@@ -11,20 +12,19 @@ public class TrumpSelect : MonoBehaviour
     public GameObject yellowSelectBtn;
     public GameObject readyBtn;
 
+
+    public delegate void TrumpUpdated(CardColor cardColor, bool isReady);
+    private List<TrumpUpdated> callbacks = new List<TrumpUpdated>();
+
     public GameObject infoTxt;
 
     private CardColor color = CardColor.NONE;
+    private int toDiscard = 5;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start() 
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        AddCallback(OnTrumpUpdated);
+        updateHelp();
     }
 
     private void resetButtons()
@@ -39,8 +39,73 @@ public class TrumpSelect : MonoBehaviour
     {
         resetButtons();
         btn.GetComponent<Button>().interactable = false;
+        updateHelp();
+        if (toDiscard == 0) {
+            readyBtn.GetComponent<Button>().interactable = true;
+        }
+        call(false);
     }
 
+    private void call(bool ready)
+    {
+        foreach (TrumpUpdated callback in callbacks) {
+            callback(color, ready);
+        }
+    }
+
+    public void AddCallback(TrumpUpdated callback)
+    {
+        callbacks.Add(callback);
+    }
+
+    public void UpdateToDiscard(int cardsLeft)
+    {
+        toDiscard = cardsLeft < 0 ? 0 : cardsLeft;
+        if (toDiscard == 0 && color != CardColor.NONE) {
+            readyBtn.GetComponent<Button>().interactable = true;
+        }
+        updateHelp();
+    }
+
+    public void DiscardOne()
+    {
+        UpdateToDiscard(toDiscard - 1);
+    }
+
+    private void updateHelp()
+    {
+        string text = "";
+
+        if (color == CardColor.NONE) {
+            text += "Select a trump color";
+        } else {
+            text += "You have selected ";
+            switch (color) {
+                case CardColor.GREEN: text += "green";
+                    break;
+                case CardColor.RED: text += "red";
+                    break;
+                case CardColor.BLACK: text += "black";
+                    break;
+                case CardColor.YELLOW: text += "yellow";
+                    break;
+                default: text += "no";
+                    break;
+            }
+            text += " trump";
+        }
+
+        if (toDiscard > 0) {
+            text += " and discard " + toDiscard + " more cards";
+        }
+
+        infoTxt.GetComponent<TextMeshProUGUI>().text = text;
+    }
+
+    public void OnTrumpUpdated(CardColor trump, bool isReady)
+    {
+        Debug.Log("Callback Called: Is ready: " + isReady);
+    }
 
     public void OnGreenSelect() 
     {
@@ -68,6 +133,17 @@ public class TrumpSelect : MonoBehaviour
         color = CardColor.YELLOW;
         disableButton(yellowSelectBtn);
         Debug.Log("Yellow selected");
+    }
+
+    public void OnReady()
+    {
+        if (color == CardColor.NONE) {
+            return;
+        }
+        if (toDiscard > 0) {
+            return;
+        }
+        call(true);
     }
 
 }
